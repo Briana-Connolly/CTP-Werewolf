e31af29d8501a59e412cb7b6fc6388a2cf39e7f5
Briana Connolly <bconnolly555@gmail.com>
2019-11-22 20:46:01 -0500

    started working on Seer screen

--- src/loneWWScreen.js
+++ src/loneWWScreen.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+
 import './start.css'
 
 //need to add functionality that once a button is clicked no other buttons can be clicked
 
-function Players(num) {
+function displayResult(num) {
     console.log();
     const middle = ['werewolf', 'minion', 'villager'];
     const result = middle[num - 1];
@@ -24,20 +25,20 @@ class LoneWWScreen extends React.Component {
         this.state.b = 0;
 
         this.toggleDiv = this.toggleDiv.bind(this)
-        
+
         this.state = {
             isButtonDisabled: false
-          }
+        }
     }
 
     toggleDiv = (num) => {
         this.state.b = num;
         const { show } = this.state;
         this.setState({ show: !show })
-        
+
         this.setState({
             isButtonDisabled: true
-          });
+        });
     }
 
     render() {
@@ -47,18 +48,18 @@ class LoneWWScreen extends React.Component {
                 <p className="description">Pick which card from the middle you would like to see</p>
                 <br></br>
                 <br></br>
-                <button className="box" 
+                <button className="box"
                     onClick={() => this.toggleDiv(1)}
                     disabled={this.state.isButtonDisabled}>Middle 1</button>
-                <button className="box" 
+                <button className="box"
                     onClick={() => this.toggleDiv(2)}
                     disabled={this.state.isButtonDisabled}>Middle 2</button>
-                <button className="box" 
+                <button className="box"
                     onClick={() => this.toggleDiv(3)}
                     disabled={this.state.isButtonDisabled}>Middle 3</button>
                 <br></br>
                 <br></br>
-                <div className="result">{this.state.show && Players(this.state.b)}</div>
+                <div className="result">{this.state.show && displayResult(this.state.b)}</div>
             </div >
         );
     }
--- src/seerScreen.js
+++ src/seerScreen.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './start.css'
+
+// Need to fix buttons
 
 class SeerScreen extends React.Component {
     componentDidMount() {
         document.body.style.background = "#F8F7FA";
+    }
+
+    constructor() {
+        super();
+
+        this.state = {
+            playerClicked: false,
+            middleClicked: false
+        };
+
+
+        this.handlePlayerClick = this.handlePlayerClick.bind(this);
+        this.handleMiddleClick = this.handleMiddleClick.bind(this);
+    }
+
+    handlePlayerClick() {
+        this.setState({
+            playerClicked: true,
+        });
+    }
+
+    handleMiddleClick() {
+        this.setState({
+            middleClicked: true,
+        });
     }
 
     render() {
@@ -12,6 +40,66 @@ class SeerScreen extends React.Component {
             <div className="minionScreen">
                 <p className="title">You are the Seer</p>
                 <p className="description">Pick which player you want to see or Pick to see 2 cards from the middle</p>
+                <br></br>
+                <br></br>
+                {this.state.playerClicked ? (
+                    <PlayerSeerScreen />
+                ) : (
+                        <button className="box" onClick={this.handlePlayerClick}> Player </button>
+                    )}
+                {this.state.middleClicked ? (
+                    <MiddleSeerScreen />
+                ) : (
+                        <button className="box" onClick={this.handleMiddleClick}> Middle </button>
+                    )}
+            </div >
+        );
+    }
+}
+
+function Players() {
+    let numPlayers = 5;
+    const roles = ['werewolf', 'minion', 'seer', 'robber', 'werewolf'];
+    const newDivs = [];
+
+    for (let i = 0; i < numPlayers; i++) {
+        //might have to change this from role to player #
+        if (roles[i] != 'seer') {
+            newDivs.push(<button className="box">Player {i + 1}</button>);
+        }
+        console.log(newDivs);
+    }
+    return newDivs;
+}
+
+class PlayerSeerScreen extends React.Component {
+    componentDidMount() {
+        document.body.style.background = "#F8F7FA";
+    }
+
+    render() {
+        return (
+            <div className="minionScreen">
+                <div>{Players()}</div>
+                <br></br>
+                {/* <div className="result">{Players()}</div> */}
+            </div >
+        );
+    }
+}
+
+class MiddleSeerScreen extends React.Component {
+    componentDidMount() {
+        document.body.style.background = "#F8F7FA";
+    }
+
+    render() {
+        return (
+            <div className="minionScreen">
+                <button className="box" >Middle 1</button>
+                <button className="box">Middle 2</button>
+                <button className="box">Middle 3</button>
+                {/* <div className="result">{Players()}</div> */}
             </div >
         );
     }
