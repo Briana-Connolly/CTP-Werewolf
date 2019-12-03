9fc4061c670820c3ae143c5175b61e234988b049
Briana Connolly <bconnolly555@gmail.com>
2019-12-03 01:01:54 -0500

    added name to home screen

--- src/home.js
+++ src/home.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import './index.css'
+import { Redirect } from 'react-router';
+
 
 import logo from './images/WWLogo.png'
-
 
 class HomeScreen extends React.Component {
     componentDidMount() {
         document.body.style.background = "#42033D";
     }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            playerName: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ playerName: event.target.value });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log(this.state.playerName);
+        this.props.history.push('/start');
+    }
+
     render() {
+
         return (
             <div>
                 <img src={logo} alt="WW logo" height="300" width="700"></img>
                 <br></br>
                 <br></br>
-                <Link to="/start" className="btn btn-primary">Start Game</Link>
-            </div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" name="name" placeholder="Enter Name" onChange={(event) => this.handleChange(event)}></input>
+                    <input type="submit" value="Start Game" />
+                </form>
+                <br></br>
+            </div >
         );
     }
 }
 
-export default HomeScreen
+export default withRouter(HomeScreen);
